b91459f0208a4231e7331569f1be2f2bf56ba7ee
iwasaki <nemuisinemuisi@gmail.com>
2016-05-20 13:59:01 +0900

    a

---
+++ eventManagement/application/views/event/show.php
@@ -0,0 +1,42 @@
+<h1>イベント詳細</h1>
+
+<table class="table">
+
+	<tbody>
+		<tr>
+			<th>タイトル</th>
+			<td>HTML5勉強会</td>
+		</tr>
+		<tr>
+			<th>開始時間</th>
+			<td>2016年06月15日(水)19時00分</td>
+		</tr>
+		<tr>
+			<th>終了時間</th>
+			<td>2016年06月15日(水)20時00分</td>
+		</tr>
+		<tr>
+			<th>場所</th>
+			<td>第1会議室</td>
+		</tr>
+		<tr>
+			<th>対象グループ</th>
+			<td>技術部</td>
+		</tr>
+    <tr>
+      <th>詳細</th>
+      <td>HTML5の勉強会を行います。
+           </br>対象はHTML4.01あるいはXHTML1.0を知っていて、JavaScriptの基本コーディングができる方です。</td>
+    </tr>
+    <tr>
+      <th>登録者</th>
+      <td>佐藤三郎</td>
+    </tr>
+    <tr>
+      <th>参加者</th>
+      <td>山田太郎,鈴木次郎,佐藤三郎</td>
+    </tr>
+	</tbody>
+</table>
+<input class="btn btn-primary" type="submit" value="一覧に戻る">
+<input class="btn btn-success" type="submit" value="参加する">
