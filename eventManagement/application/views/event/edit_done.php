9a88c2e440e0e142cdaf0c5020e2df91a462d3f2
sakuoka <12arusu28@gmail.com>
2016-05-20 13:05:59 +0900

    saihensyuu

--- eventManagement/application/views/event/edit_done.php
+++ eventManagement/application/views/event/edit_done.php
@@ -1,17 +1,6 @@
 <body>
 
- <div class="btn-group btn-group-justified">
-    	<a class="btn btn-default">EventManager</a>
-        <a class="btn btn-primary">本日のイベント</a>
-        <a class="btn btn-success">イベント管理</a>
-        <a class="btn btn-info">ユーザー管理</a>
-        <a class="btn btn-warning">
-        <select name="login" color="black">
-		<option>ログイン者</option>
-		<option>ログアウト</option>
-		</select>
-		</a>
-    </div>
+<!-- ヘッダー-->
 
 <h1>イベント編集</h1>
 <p>イベントの編集が完了しました。</p>
