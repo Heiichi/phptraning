e11af81f9d22797e2d3e1181034428b8f75f06b2
Heiichi Sato <heiichisato@gmail.com>
2016-05-26 17:10:39 +0900

    change user/index

--- eventManagement/application/views/user/edit_done.php
+++ eventManagement/application/views/user/edit_done.php
@@ -1,12 +1,11 @@
 <div class="container">
   <div class="panel panel-default">
     <div class="panel-heading">
-      event Manager
+      ユーザ編集
     </div>
     <div class="panel-body">
       <div class="row">
         <div class="col-md-12">
-          <h1>ユーザ編集</h1>
           <p>ユーザ編集が完了しました。</p>
           <p><a href="index">ユーザ一覧に戻る</a></p>
         </div>
--- eventManagement/application/views/user/index.php
+++ eventManagement/application/views/user/index.php
@@ -1,50 +1,57 @@
 <div class="container">
-  <h1>ユーザ一覧</h1>
-  <div class="table-responsive">
-    <nav>
-      <?php echo $this->pagination->create_links(); ?>
-    </nav>
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>氏名</th>
-          <th>所属グループ</th>
-          <th>詳細</th>
-          <th>削除</th>
-        </tr>
-      </thead>
-      <?php foreach ($users as $user): ?>
-        <tr>
-          <td><?php echo $user->id; ?></td>
-          <td><?php echo $user->uname; ?></td>
-          <td><?php echo $user->name; ?></td>
-          <td><a class="btn btn-primary" href="<?php echo base_url('user/show/'.$user->id); ?>">詳細</a></td>
-          <td>
-            <a class="btn btn-danger" href="#" role="button" data-toggle="modal" data-target="#userModal">削除
-            </a>
+  <div class="panel panel-default">
+    <div class="panel-heading">ユーザ一覧</div>
+  <div class="panel-body">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="table-responsive">
+          <nav>
+            <?php echo $this->pagination->create_links(); ?>
+          </nav>
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>氏名</th>
+                <th>所属グループ</th>
+                <th>詳細</th>
+                <th>削除</th>
+              </tr>
+            </thead>
+            <?php foreach ($users as $user): ?>
+              <tr>
+                <td><?php echo $user->id; ?></td>
+                <td><?php echo $user->uname; ?></td>
+                <td><?php echo $user->name; ?></td>
+                <td><a class="btn btn-primary" href="<?php echo base_url('user/show/'.$user->id); ?>">詳細</a></td>
+                <td>
+                  <a class="btn btn-danger" href="#" role="button" data-toggle="modal" data-target="#userModal">削除
+                  </a>
 
-            <!-- モーダルウィンドウ -->
-            <div class="modal fade" id="userModal">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-body">
-                    本当に削除してよろしいですか?
+                  <!-- モーダルウィンドウ -->
+                  <div class="modal fade" id="userModal">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-body">
+                          本当に削除してよろしいですか?
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">
+                            Cancel
+                          </button>
+                          <a href="<?php echo base_url('user/delete/' . $user->id); ?>"><button type="button" class="btn btn-success">OK</button></a>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">
-                      Cancel
-                    </button>
-                    <a href="<?php echo base_url('user/delete/' . $user->id); ?>"><button type="button" class="btn btn-success">OK</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- モーダルウィンドウend -->
-          </td>
-        </tr>
-      <?php endforeach; ?>
-    </table>
-    <a class="btn btn-primary" href="<?php echo base_url('user/add'); ?>">ユーザの登録</a>
+                  <!-- モーダルウィンドウend -->
+                </td>
+              </tr>
+            <?php endforeach; ?>
+          </table>
+          <a class="btn btn-primary" href="<?php echo base_url('user/add'); ?>">ユーザの登録</a>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
