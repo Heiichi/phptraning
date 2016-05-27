8e766a137742f696232137340d7d3eebce2a3744
Naojirou Hisada <NaojirouHisada@gmail.com>
2016-05-27 15:19:38 +0900

    イベント一覧、本日のイベント、グループ一覧、グループ追加、お知らせ、連絡事項のレイアウト修正、および、連絡事項追加のバリデーションの追加

--- eventManagement/application/views/event/index.php
+++ eventManagement/application/views/event/index.php
@@ -9,40 +9,48 @@
 
 
 <div class="container">
-  <h1 id="show-event">イベント一覧</h1>
-  <div id="pages">
-    <?php echo $this->pagination->create_links();?>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      イベント一覧
+    </div>
+    <div class="panel-body">
+      <div class="row">
+
+       <div class="col-md-9">
+        <h1 id="show-event">イベント一覧</h1>
+        <div id="pages">
+          <?php echo $this->pagination->create_links();?>
+        </div>
+        <table class="table table-bordered" >
+          <thead>
+            <tr>
+              <th>タイトル</th>
+              <th>開始日時</th>
+              <th>場所</th>
+              <th>対象グループ</th>
+              <th>詳細</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach($events as $event): ?>
+              <tr>
+                <?php if(in_array($event->id,$check,true) || $event->registered_by === $register): ?>
+                  <th><?php echo $event->title; ?><span class="label label-info spanlabel">参加</span></th>
+                <?php else: ?>
+                  <th><?php echo $event->title; ?></th>
+                <?php endif; ?>
+                <td><?php echo $event->start; ?></td>
+                <td><?php echo $event->place; ?></td>
+                <td><?php echo $event->name; ?></td>
+                <td><a href="<?php echo base_url('event/show/'.$event->id); ?>"class="btn btn-default" href="#" role="button">詳細</a></td>
+              </tr>
+            <?php endforeach; ?>
+
+          </tbody>
+        </table>
+
+        <a href="<?php echo base_url('event/add'); ?>"><button class="btn btn-primary">イベント登録</button></a>
+      </div>
+    </div>
   </div>
-  <table class="table table-bordered" >
-    <thead>
-      <tr>
-        <th>タイトル</th>
-        <th>開始日時</th>
-        <th>場所</th>
-        <th>対象グループ</th>
-        <th>詳細</th>
-      </tr>
-    </thead>
-    <tbody>
-
-
-
-      <?php foreach($events as $event): ?>
-        <tr>
-          <?php if(in_array($event->id,$check,true) || $event->registered_by === $register): ?>
-            <th><?php echo $event->title; ?><span class="label label-info spanlabel">参加</span></th>
-          <?php else: ?>
-            <th><?php echo $event->title; ?></th>
-          <?php endif; ?>
-          <td><?php echo $event->start; ?></td>
-          <td><?php echo $event->place; ?></td>
-          <td><?php echo $event->name; ?></td>
-          <td><a href="<?php echo base_url('event/show/'.$event->id); ?>"class="btn btn-default" href="#" role="button">詳細</a></td>
-        </tr>
-      <?php endforeach; ?>
-
-    </tbody>
-  </table>
-
-  <a href="<?php echo base_url('event/add'); ?>"><button class="btn btn-primary">イベント登録</button></a>
 </div>
--- eventManagement/application/views/group/add.php
+++ eventManagement/application/views/group/add.php
@@ -1,14 +1,31 @@
 <div class="container">
-　　<h1>グループ登録</h1>
   <div class="row">
-    <fiedlset class="form-group">
-      <?php echo form_open(); ?>
-        <?php echo form_label('グループ名<span>*</span>',"group"); ?>
-        <?php echo form_input('name',set_value('name'),'class="form-control" placeholder="グループ名" ') ;?>
-          <div class="help-block with-errors"><?php echo form_error('name','<p>','</p>');?></div>
-    </fiedlset>
-    <?php echo form_submit('cancel','キャンセル','class="btn btn-default"');?>
-    <?php echo form_submit('add','登録','class="btn btn-success"');?>
-    <?php echo form_close(); ?>
+    <div class="col-md-9">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          グループ一覧
+        </div>
+
+      <div class="panel-body ">
+          <div class="container">
+          　　<h1>グループ登録</h1>
+
+            <fiedlset class="form-group">
+              <?php echo form_open(); ?>
+                <?php echo form_label('グループ名<span>*</span>',"group"); ?>
+                <div class="row">
+                  <div class="col-xs-6">
+                  <?php echo form_input('name',set_value('name'),'class="form-control" placeholder="グループ名" ') ;?>
+                    <div class="help-block with-errors"><?php echo form_error('name','<p>','</p>');?></div>
+                  </div>
+                </div>
+            </fiedlset>
+            <?php echo form_submit('cancel','キャンセル','class="btn btn-default"');?>
+            <?php echo form_submit('add','登録','class="btn btn-success"');?>
+            <?php echo form_close(); ?>
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
