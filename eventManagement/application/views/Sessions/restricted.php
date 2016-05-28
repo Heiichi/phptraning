7de423e97283696012157fa6dad72c3a9caa97b7
Heiichi Sato <h.sato@m.yell4u.jp>
2016-05-28 09:23:19 +0900

    [WIP]メールでの新規登録機能

--- eventManagement/application/views/Sessions/restricted.php
+++ eventManagement/application/views/Sessions/restricted.php
@@ -6,7 +6,7 @@
     <div class="panel-body">
       <div class="row">
         <div class="col-md-12">
-          <h1>アクセス権限がありません。</h1>
+          <h1>アクセス権限がありませぬ。</h1>
           <p><a href="<?php echo base_url() . "sessions/login" ?>">ログイン画面に戻る</a></p>
         </div>
       </div>
